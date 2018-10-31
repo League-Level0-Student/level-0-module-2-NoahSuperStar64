fe651c63d7d2ca60da7f026e02db69a7d718387f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-31 12:59:52 -0700

    Worked on module 2 projects

--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -6,30 +6,37 @@ package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+Random gen = new Random();
+int Number = gen.nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(Number);
 	// 4. Get the user to enter a question for the 8 ball
-
+JOptionPane.showInputDialog("Asd akmiitikmg, akmk I çal çare nii dkmovvlekge vvit jou† † † )
 	// 5. If the random number is 0
-
+if(Number == 0) {
 	// -- tell the user "Yes"
-
+JOptionPane.showMessageDialog(null, "Jes");
+}
 	// 6. If the random number is 1
-
+else if(Number == 1) {
 	// -- tell the user "No"
-
+JOptionPane.showMessageDialog(null, "Kmaux");
+}
 	// 7. If the random number is 2
-
+else if(Number == 2) {
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+}
 }
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,12 +12,29 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+	
 		
-		System.out.println(randomNumber);
+		
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+       for(int i = 0; i < 11; i++) {
+    	   int randomNumber = randomMaker.nextInt(5);
+		if(randomNumber == 0) {
+        	JOptionPane.showMessageDialog(null, "You look 20% cooler than ever!");
+        }
+        else if(randomNumber == 1) {
+        	JOptionPane.showMessageDialog(null, "Jou lod 20% koler takm ewer‡");
+        }
+        else if(randomNumber == 2) {
+        	JOptionPane.showMessageDialog(null, "No compliment for you!");
+        }
+        else if(randomNumber == 3) {
+        	JOptionPane.showMessageDialog(null, "You are the very best like no one ever was");
+        }
+        else {
+        	JOptionPane.showMessageDialog(null, "you dj veer goo awg shoutout to djpengun check out his channel the jamming gamer for top notch minecraft story mode gameplay");
+        }
+       }
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
